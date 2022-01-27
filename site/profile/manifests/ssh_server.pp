bf137c16cddbfac48541399e579b962e409eabed
Diartenyx <70727628+Diartenyx@users.noreply.github.com>
2022-01-27 16:45:38 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDL9HnFdHquv/QbUGHbGbd2Uafuqhy6kq7tBmEFh8ozcxpXleqNtBRsbCB88HBb7EArp+wHItg7Laj9rBUwFi3RUg9143iHZN753i4268H7NB+u0Z1B6TJBMSW/7KBZCIyzmYxZqIuHb6jW5tUlTRX6dXK4a7/aFxzkqEzBYl7iaEHLLHRMV+d9Tujmb4wuJetfhsFI3WL0UTBYnkS2CrBvmJAqG7/QjBuPCXnl0RurLvUeaXSkdx66Cex407Fbkub+kT9DwVaGWxN5vuW2MzvTO0zR1493rbpGrUEdche0KltjxXP2DMCCg//TWpXdDvwCANF50VQ1NDjW0LPEhGFR',
 	}  
 }
